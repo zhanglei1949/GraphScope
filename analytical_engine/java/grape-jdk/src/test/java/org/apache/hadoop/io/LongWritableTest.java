f578ee36116c0501f157877c69de6450245c2795
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2021-11-30 11:17:50 +0800

    try another

--- analytical_engine/java/grape-jdk/src/test/java/org/apache/hadoop/io/LongWritableTest.java
+++ analytical_engine/java/grape-jdk/src/test/java/org/apache/hadoop/io/LongWritableTest.java
@@ -2,6 +2,7 @@ package org.apache.hadoop.io;
 
 import com.alibaba.fastffi.CXXValueScope;
 import java.util.concurrent.TimeUnit;
+import org.apache.hadoop.io.ffi.FFILongWritable;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -19,13 +20,17 @@ public class LongWritableTest {
 
     private LongWritable[] longWritables;
     private LongWritable[] readWritable;
+    private FFILongWritable[] ffiLongWritables;
     private int SIZE = 1024 * 1024;
 
     @Setup
     public void prepare() {
         readWritable = new LongWritable[SIZE];
+        ffiLongWritables = new FFILongWritable[SIZE];
         for (int i = 0; i < SIZE; ++i) {
             readWritable[i] = new LongWritable(i);
+            ffiLongWritables[i] = FFILongWritable.factory.create();
+            ffiLongWritables[i].value(i);
         }
     }
 
@@ -57,10 +62,30 @@ public class LongWritableTest {
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void readFFI() {
+        int result = 0;
+        for (int i = 0; i < SIZE; ++i) {
+            result += ffiLongWritables[i].value();
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
     public void write() {
         int result = 0;
         for (int i = 0; i < SIZE; ++i) {
             readWritable[i].set(i);
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void writeFFI() {
+        int result = 0;
+        for (int i = 0; i < SIZE; ++i) {
+            ffiLongWritables[i].value(i);
         }
     }
 }
