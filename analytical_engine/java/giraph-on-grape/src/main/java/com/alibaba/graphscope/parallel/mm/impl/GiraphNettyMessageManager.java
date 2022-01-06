a3154c6dec7b93763159ec991b41c2667b6a1939
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2022-01-06 10:39:15 +0800

    fix cache

--- analytical_engine/java/giraph-on-grape/src/main/java/com/alibaba/graphscope/parallel/mm/impl/GiraphNettyMessageManager.java
+++ analytical_engine/java/giraph-on-grape/src/main/java/com/alibaba/graphscope/parallel/mm/impl/GiraphNettyMessageManager.java
@@ -17,6 +17,7 @@ import com.alibaba.graphscope.parallel.netty.NettyClient;
 import com.alibaba.graphscope.parallel.netty.NettyServer;
 import com.alibaba.graphscope.parallel.netty.request.impl.GidLongWritableRequest;
 import com.alibaba.graphscope.parallel.utils.NetworkMap;
+import com.alibaba.graphscope.utils.FFITypeFactoryhelper;
 import java.io.IOException;
 import java.util.Iterator;
 import org.apache.giraph.conf.ImmutableClassesGiraphConfiguration;
@@ -100,6 +101,7 @@ public class GiraphNettyMessageManager<
         initMessageStore();
         //Netty server depends on message store.
         initNetty();
+        grapeVertex = FFITypeFactoryhelper.newVertex(conf.getGrapeVidClass());
     }
 
     public void initNetty(){
@@ -172,7 +174,9 @@ public class GiraphNettyMessageManager<
         if (dstOid instanceof LongWritable) {
             long longOid = ((LongWritable) dstOid).get();
             // Get lid from oid
-            fragment.getInnerVertex(longOid, grapeVertex);
+
+            assert fragment.getInnerVertex(longOid, grapeVertex);
+            logger.debug("oid 2 lid: " + longOid + ", " + grapeVertex.GetValue());
 
             int dstfragId = fragment.getFragId(grapeVertex);
             logger.debug("Message manager sending via cache, dst: " + dstfragId + "msg: " + message);
