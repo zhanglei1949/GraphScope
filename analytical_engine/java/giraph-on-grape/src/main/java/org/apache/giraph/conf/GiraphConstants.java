154928fd33ed9c4f752aac64a0603f475e5fd401
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2022-01-18 12:40:36 +0800

    f

--- analytical_engine/java/giraph-on-grape/src/main/java/org/apache/giraph/conf/GiraphConstants.java
+++ analytical_engine/java/giraph-on-grape/src/main/java/org/apache/giraph/conf/GiraphConstants.java
@@ -34,7 +34,7 @@ public interface GiraphConstants {
      */
     int ONE_MB = 1024 * 1024;
 
-    int clientCacheSize = 4 * ONE_MB;
+    int clientCacheSize = 128 * ONE_MB;
 
     /**
      * VertexOutputFormat class
