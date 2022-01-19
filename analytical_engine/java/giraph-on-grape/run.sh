608cf741ba934dcdb6280fc1d1a4c7e02dc664a0
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2022-01-19 20:48:21 +0800

    show log

--- analytical_engine/java/giraph-on-grape/run.sh
+++ analytical_engine/java/giraph-on-grape/run.sh
@@ -16,16 +16,16 @@ GLOG_v=10 mpirun \
 
 # datagen
 GLOG_v=2 mpirun \
--n 2 \
+-n 4 \
 -envlist MESSAGE_STORE_TYPE,GLOG_v,GRAPE_JVM_OPTS,USER_JAR_PATH,GRAPH_TYPE,APP_TYPE,MAX_SUPER_STEP,MESSAGE_MANAGER_TYPE,OUT_MESSAGE_CACHE_TYPE,MESSAGE_STORE_TYPE \
 ./giraph_runner --app_class com.alibaba.graphscope.samples.MessageBenchMark \
---efile ./lei.e --vfile ./lei.v \
+--efile ~/libgrape-lite/dataset/p2p-31.e --vfile ~/libgrape-lite/dataset/p2p-31.v \
 --worker_context_class com.alibaba.graphscope.samples.MessageBenchMark\$MessageBenchMarkWorkerContext \
 --lib_path /opt/graphscope/lib/libgiraph-jni.so --loading_thread_num 1 \
 --serialize true --serialize_prefix p2p --grape_loader true
 
 
---efile ~/libgrape-lite/dataset/p2p-31.e --vfile ~/libgrape-lite/dataset/p2p-31.v \
+--efile ./lei.e --vfile ./lei.v \
 # com 
 GLOG_v=10 mpirun \
 -n 4 \x
