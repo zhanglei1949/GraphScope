20b9268e0ed413e0e72679cc6a3e936ccc2dcaa9
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2025-02-25 15:01:45 +0800

    Revert "to test use stringmap on qidian"
    This reverts commit 2ae1b3ac4ec3239f2598ae083db23715fbb2dbcb.

--- flex/interactive/examples/qidian/plugins/build_plugin.sh
+++
@@ -1,9 +0,0 @@
-ARCH=`uname -m`
-#FLEX_HOME = / usr / local
-FLEX_HOME=/opt/flex/
-
-#for i in ctrl group minor 
-for i in qidian
-do
-  g++ -flto -fPIC -finline-functions -zopt -march=native --std=c++17  -I/usr/lib/${ARCH}-linux-gnu/openmpi/include -I${FLEX_HOME}/include -I/opt/graphscope/include -L${FLEX_HOME}/lib -rdynamic -O3 -o lib${i}.so ${i}.cc -lflex_utils -lflex_rt_mutable_graph -lflex_graph_db -shared
-done
